d81f9cd11c991ce354a810a4ded09e7dfb406fd6
Ben Tidwell <ben.tidwell@careerbuilder.com>
2015-02-13 15:25:31 -0500

    proof of concept for crawling.
    Sitemap building to come

--- crawl.js
+++ crawl.js
@@ -1,9 +1,12 @@
 var pages = [];
 var Crawler = require("simplecrawler");
-var excludedPaths = [
+var LINQ = require('node-linq').LINQ;
+var excludedCrawlPaths = [
     '/api/localization/script',
     '/public/'
 ];
+
+var excludedSiteMapPaths = ['/search', '/join'];
 var myCrawler = new Crawler("pencilblue-548349876.us-east-1.elb.amazonaws.com", "/search");
 var conditionID = myCrawler.addFetchCondition(function(parsedURL) {
     return pathIsNotAFile(parsedURL.path) && pathIsNotInExclusionList(parsedURL.path);
@@ -13,10 +16,20 @@ myCrawler.stripQuerystring = false;
 myCrawler.maxConcurrency = 50;
 myCrawler.on("fetchcomplete",function(queueItem, data, res){
     var path = stripQueryString(queueItem.path);
-    if(pathIsNotAFile(path) && pathIsNotInExclusionList(path)){
-        pages.push(stripQueryString(queueItem.url));
+    if(pathIsNotAFile(path) && pathIsNotInExclusionSiteMapList(path)){
+        pages.push({
+                url: stripQueryString(queueItem.url),
+                priority: getPagePriority(queueItem)
+        });
     }
 });
+
+function getPagePriority(queueItem){
+    if(queueItem.path.indexOf('/job/') === 0){
+        return 0.1;
+    }
+    return 0.3;
+}
 
 myCrawler.on("complete", function(){
     console.log(pages);
@@ -35,9 +48,17 @@ function pathIsNotAFile(path){
     return path.match(/^[^.]+$|\.(?!(js|css|woff|tff|ttf|svg|eot|ico|jpg|png|bmp|gif|xml|json)$)([^.]+$)/);
 }
 function pathIsNotInExclusionList(path){
-    return excludedPaths.indexOf(path) === -1;
+    return excludedCrawlPaths.indexOf(path) === -1;
+}
+function pathIsNotInExclusionSiteMapList(path){
+    return excludedSiteMapPaths.indexOf(path) === -1;
 }
 
+function urlIsNotAlreadyInSiteMap(url){
+    return new LINQ(pages).Where(function(page){
+        return page.url === url;
+    }).toArray().length === 0;
+}
 //Functional
 function stripQueryString(url){
     return url.substr(0, url.indexOf("?"));
